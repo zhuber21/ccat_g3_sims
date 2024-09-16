adea337ac261643d07e8edbc98cf8f26d9c9af4a
Andy Yang <hy586@swt8.swt.com>
2024-09-16 17:56:19 -0400

    added int32 option and changed timestamp implementation

--- g3_simulator.py
+++ g3_simulator.py
@@ -17,7 +17,7 @@ def make_obs_frame(frame_num,start,board_id):
         frame['stream_placement'] = core.G3String("stop")
     return frame
 
-def make_data_frame(frame_num, board_id, agg_time=2.0, t_samp=1.0/488.0, n_chs=2, quanta=0.001):
+def make_data_frame(frame_num, board_id, agg_time=2.0, t_samp=1.0/488.0, n_chs=2, dtype='float32', quanta=0.001, start_time=time.time()):
     """Makes a scan frame to hold timestream in the G3SuperTimestream object from so3g.
        Takes in an integer for the frame number, a board identifer string, and an integer for the
        number of resonator channels (n_chs).
@@ -26,24 +26,33 @@ def make_data_frame(frame_num, board_id, agg_time=2.0, t_samp=1.0/488.0, n_chs=2
        number of points is set by the time between each sample (t_samp = 1/f_samp where f_samp is
        set to 488 Hz by the RFSoC).
 
+       start_time indicates what the first timestamp for the timestream should be in Unix time.
+       
+       Data type stored inside g3 files are specified by parameter dtype. Possible dtypes are int32
+       float 32.
+
        Quanta sets the data precision that will be saved for the float data under compression.
        See so3g docs (https://so3g.readthedocs.io/en/latest/cpp_objects.html#how-to-work-with-float-arrays) for more info.  
        
     """
-    times = time.time() + t_samp * np.arange(agg_time / t_samp)
+    times = start_time + t_samp * np.arange(agg_time / t_samp)
     ch_names = ["ch"+str(i).zfill(3) for i in range(1,n_chs+1)]
     endings = ["_I","_Q"]
     chs = [ch_name+ending for ch_name in ch_names for ending in endings]
-    data = (np.random.normal(size=(len(chs),len(times))) * 100).astype('float32')
+
+    data = (np.random.normal(size=(len(chs),len(times))) * 100).astype(dtype)
     
     ts = so3g.G3SuperTimestream()
     ts.names = chs
     ts.times = core.G3VectorTime(times * core.G3Units.s)
-    ts.quanta = quanta * np.ones(len(chs)) # this is related to the data level to keep for the float under compression
+
+    if dtype == 'float32':
+        ts.quanta = quanta * np.ones(len(chs)) # this is related to the data level to keep for the float under compression
+    
     ts.data = data
     
     frame = core.G3Frame(core.G3FrameType.Scan)
-    frame['time'] = core.G3Time(time.time() * core.G3Units.s).time
+    frame['time'] = core.G3Time((start_time+agg_time) * core.G3Units.s).time
     frame['board_id'] = core.G3String(board_id)
     frame['data'] = ts
     frame['num_samples'] = core.G3Int(len(ts.times))
@@ -65,7 +74,7 @@ def load_timestream_from_sim_g3(fname):
     for i in range(len(frames)):
         if(frames[i].type==core.G3FrameType.Scan):
             to_store.append(frames[i]['data'])
-    
+
     combined_times = np.hstack(list(to_store[i].times for i in range(len(to_store))))
     combined_data = np.hstack(list(to_store[i].data for i in range(len(to_store))))
     
--- simulate_ccat_g3.py
+++ simulate_ccat_g3.py
@@ -3,23 +3,39 @@
     Written 9/10/24, ZBH
 """
 import g3_simulator as sim
+import so3g
 from spt3g import core
+import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 board_id = 'r1c1'          # simulating rfsoc 1 channel 1
-timestream_length = 1.0   # simulating 60 second timestream
+timestream_length = 60     # simulating 60 second timestream
 agg_time = 2.0             # 2 seconds per frame
 n_chs = 576                # simulating 576 channels of I and Q data
 
-save_dir = "/home/pc/Documents/g3_files/"
+save_dir = "./sim_data/"
 n_frames = int(timestream_length / agg_time)
 
-output_fname = save_dir+"one_second_576_chs_smaller_quanta_test.g3"
+output_fname = save_dir+"newtimestamp.g3"
 
 writer = core.G3Writer(filename=output_fname,append=True)
 writer(sim.make_obs_frame(frame_num=0,start=True,board_id=board_id))
+t_now = time.time()
 for i in range(1, n_frames+1):
-    writer(sim.make_data_frame(i,board_id,n_chs=n_chs,quanta=0.01))
-    time.sleep(agg_time)
+    writer(sim.make_data_frame(i,board_id,n_chs=n_chs,quanta=0.01, start_time=t_now))
+    t_now += agg_time
 writer(sim.make_obs_frame(frame_num=n_frames+1,start=False,board_id=board_id))
 writer(core.G3Frame(core.G3FrameType.EndProcessing))
 writer = None
+ 
+ts = sim.load_timestream_from_sim_g3('./sim_data/newtimestamp.g3')
+data = np.array(ts[2])
+
+res_num = 0
+plt.plot(ts[0]-ts[0][0], ts[2][res_num*2][:], label=f'{ts[1][res_num]} I')
+plt.plot(ts[0]-ts[0][0], ts[2][res_num*2+1][:], label=f'{ts[1][res_num]} Q')
+
+plt.savefig('./plots/IQ_view')
+print(data.shape)
+print(np.diff(ts[0]))
